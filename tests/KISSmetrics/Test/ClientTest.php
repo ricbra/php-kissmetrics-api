6f8156a2632a90251014ace67cbc9d4b6fc72827
Richard van den Brand <richard@vandenbrand.org>
2014-12-12 12:48:42 +0100

    License stuff fixed

--- tests/KISSmetrics/Test/ClientTest.php
+++ tests/KISSmetrics/Test/ClientTest.php
@@ -1,6 +1,8 @@
 <?php
 /*
- * (c) Waarneembemiddeling.nl
+ * This file is part of the php-kissmetrics-api.
+ *
+ * (c) Richard van den Brand <richard@vandenbrand.org>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the php-kissmetrics-api.
+ *
+ * (c) Richard van den Brand <richard@vandenbrand.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 date_default_timezone_set('Europe/Amsterdam');
 
